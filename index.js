5d3ea09a27b11978729df1d247293ee81a2130f3
Tommy Luo <Tommy_Creatine@Tommys-MacBook-Pro.local>
2017-10-22 04:58:17 -0400

    commit

--- index.js
+++ index.js
@@ -69,6 +69,8 @@ app.post('/api/getRating', function(req, res){
   }
 })
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
 });
